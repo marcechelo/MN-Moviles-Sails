7e4d25d67b565bd9d173571c9c4c7fbb35ad8bb8
marcelo nieto <marcechelo25@hotmail.com>
2018-06-09 08:53:33 -0500

    nueva actividad

---
+++ app/src/main/java/com/example/usrdel/servidores/HttpFuel.kt
@@ -0,0 +1,12 @@
+package com.example.usrdel.servidores
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HttpFuel : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_http_fuel)
+    }
+}
