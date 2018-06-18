a1f0e375379141faf8b696fb59ff840fa9b6a36d
Marcelo Nieto <marcechelo25@hotmail.com>
2018-06-18 08:09:05 -0500

    async

--- app/src/main/java/com/example/usrdel/servidores/AnkorActivity.kt
+++ app/src/main/java/com/example/usrdel/servidores/AnkorActivity.kt
@@ -4,18 +4,27 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.github.kittinunf.fuel.httpGet
+import kotlinx.coroutines.experimental.Deferred
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.bg
+import org.jetbrains.anko.toast
 
 class AnkorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ankor)
-
+        Log.i("async", "Comenzar")
         async(UI){
             llamarPosts("i")
             Log.i("async", "Luego de llamar post")
+
+            val data: Deferred<String> = bg {
+                // Runs in background
+                obtenerSesion()
+            }
+            mostrarToast(data.await())
         }
         Log.i("async", "Termino")
     }
@@ -27,5 +36,13 @@ class AnkorActivity : AppCompatActivity() {
             Log.i("async", " response: $response")
             Log.i("async", " result: $result")
         }
+    }
+
+    fun obtenerSesion():String{
+        return "Esta es una sesion"
+    }
+
+    fun mostrarToast(mensaje:String){
+        toast(mensaje)
     }
 }
